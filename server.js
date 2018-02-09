1daaf4c787be2da849760b8d8d954c33fa3d3c63
m44.soroush <m44.soroush@yahoo.com>
2018-02-09 20:26:18 +0330

    testing

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
 const express=require('express');
 const hbs=require('hbs');
-const app=express();
 const fs=require('fs');
+
+const app=express();
 
 const port=process.env.PORT || 3000;
 
-app.set('view engine',hbs);
+// app.set('view engine',hbs);
+
 app.use(express.static(__dirname+'/public'));
 
-app.use((req,res,next)=>{
-    const log=`${new Date().toTimeString()} => ${req.method} ${req.url}`;
-    fs.appendFile('server.txt',log+'\n',(err)=>{
-        console.log('error occurred: '+err);
-    });
+// app.use((req,res,next)=>{
+//     const log=`${new Date().toTimeString()} => ${req.method} ${req.url}`;
+//     fs.appendFile('server.txt',log+'\n',(err)=>{
+//         console.log('error occurred: '+err);
+//     });
+//
+//     next();
+// });
 
-    next();
+// app.get('/about',(req,res)=>{
+//     res.render('about.hbs');
+// });
+
+
+
+app.get('*',(req,res)=>{
+    res.sendFile(__dirname+'/public/index.html');
 });
 
-app.get('/',(req,res)=>{
-    res.render('home.hbs',{
-        msg:'Welcome to home'
-    });
-});
-
-app.get('/about',(req,res)=>{
-    res.render('about.hbs');
-});
-
-app.get('/wlc',(req,res)=>{
-    res.render('wlc.hbs');
-});
 
 app.listen(port,()=>{
     console.log(`Server is up on port ${port}`);
